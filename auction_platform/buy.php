7069fbd9b4424861ef453c37e26849a43d6f33b7
3shoush <35774562+3shoush@users.noreply.github.com>
2018-02-26 05:38:39 +0000

    Add files via upload

---
+++ auction_platform/buy.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include ('template/include_files.php');?>
+  <script type="text/javascript" src="js/searched_item.js"></script>
+</head>
+<body>
+  <div class="container">
+    <?php include ('template/navbar.php');?>
+    <div style="display:none" id="success-info" class="alert alert-success" role="alert"></div>
+    <div style="display:none" id="danger-info" class="alert alert-danger" role="alert"></div>
+    <nav class="navbar navbar-light bg-light">
+      <form class="form-inline">
+        <input id="searched_item_input" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="button" onclick="searchItems()" class="btn btn-outline-success my-2 my-sm-0">Search</button>
+      </form>
+    </nav>
+    <br>
+  <?php include ('template/searched_items.php');?>
+<!-- <a class="align-right" href="#">More</a> -->
+<!-- <br> -->
+   
+    <strong class="d-inline-block mb-2 text-primary">Categories</strong>
+    <div class="row">
+      <div class="col">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_161b5a3c5f4%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_161b5a3c5f4%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+          <strong class="d-inline-block mb-2 text-danger align-center">Loose Furniture</strong>
+          <div class="card-body">
+            
+            <p class="card-text">Beautiful long mirror can stand elegantly in any dressing room.</p>
+            <div class="d-flex justify-content-between align-items-center">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_161b5a3c5f4%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_161b5a3c5f4%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+          <strong class="d-inline-block mb-3 text-info align-center swatch-indigo">Paintings</strong>
+          <div class="card-body">
+            
+            <p class="card-text">Beautiful long mirror can stand elegantly in any dressing room.</p>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_161b5a3c5f4%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_161b5a3c5f4%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+          <strong class="d-inline-block mb-2 text-secondary align-center">Sculpture</strong>
+          <div class="card-body">
+            
+            <p class="card-text">Beautiful long mirror can stand elegantly in any dressing room.</p>
+            
+          </div>
+        </div>
+        <a class="align-right" href="#">More</a>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
